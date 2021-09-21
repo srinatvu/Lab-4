8f4c569cae525e361983a04c91ff44eb5db9d9c1
srinatvu <89589813+srinatvu@users.noreply.github.com>
2021-09-21 23:24:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+/*
+Lab 4
+Vishnu Srinath
+Date: Sept. 21
+*/
+
+import java.util.Random;
+
+class Main {
+  public static void main(String[] args) {
+Random r = new Random();
+int randomNum = r.nextInt(50) + 1;
+System.out.println("The Random number is: " + randomNum);
+for (int i = randomNum;i>=0;i--){
+  System.out.println(i + "\n");
+}
+if (randomNum <= 5){
+  System.out.println("Ahoy mateys!");
+}
+if(randomNum > 25 && randomNum < 42){
+  System.out.println("Cannonball!");
+}
+  }
+}
